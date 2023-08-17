2af4855f03e54ce514b6f8d99b0295387a62b6ed
Futaba3 <nyokinyoki41aya@gmail.com>
2023-08-18 00:15:26 +0900

    Initial Commit

---
+++ RealmKakeibo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmKakeibo
+//
+//  Created by Ayana Kudo on 2023/08/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
