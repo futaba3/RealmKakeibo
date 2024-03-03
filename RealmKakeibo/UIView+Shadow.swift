21d350ffb24feaf5afb8b50a92b6f3649dac35b3
futaba3 <nyokinyoki41aya@gmail.com>
2024-03-03 14:07:07 +0900

    Shadowのextensionを追加

---
+++ RealmKakeibo/UIView+Shadow.swift
@@ -0,0 +1,19 @@
+//
+//  UIView+Shadow.swift
+//  RealmKakeibo
+//
+//  Created by Ayana Kudo on 2024/03/03.
+//
+
+import UIKit
+
+extension UIView {
+    func applyShadow(cornerRadius: CGFloat) {
+        layer.cornerRadius = cornerRadius
+        layer.masksToBounds = false
+        layer.shadowRadius = 4.0
+        layer.shadowOpacity = 0.30
+        layer.shadowColor = UIColor.gray.cgColor
+        layer.shadowOffset = CGSize(width: 0, height: 5)
+    }
+}
