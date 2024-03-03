3f837561364fe8c353315c3a0265efc90ce48c04
futaba3 <nyokinyoki41aya@gmail.com>
2024-03-03 14:38:58 +0900

    コメントアウトを追加、cellの下のpaddingを削除するためautoLayoutを変更

--- RealmKakeibo/UIView+Shadow.swift
+++ RealmKakeibo/UIView+Shadow.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 extension UIView {
+    // UIViewに影を追加する
     func applyShadow(cornerRadius: CGFloat) {
+        // 引数で渡されたcornerRadiusを使って角丸にする
         layer.cornerRadius = cornerRadius
+        // viewのレイヤーに合わせて角に対して行った効果(CornerRadius)をクリッピングマスクするかどうか
         layer.masksToBounds = false
+        // 影の幅(値が大きいほどぼかsしが効く)
         layer.shadowRadius = 4.0
+        // 影の不透明度(値が小さいほど色が薄くなるs)
         layer.shadowOpacity = 0.30
         layer.shadowColor = UIColor.gray.cgColor
-        layer.shadowOffset = CGSize(width: 4, height: 4)
+        // Viewから影までの距離(width分右に、height分下にずれる)
+        layer.shadowOffset = CGSize(width: 0, height: 2)
     }
 }
